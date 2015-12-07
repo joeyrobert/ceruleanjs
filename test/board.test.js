bbaf2cc6c764a9019de83acea136d4168441c98c
joeyrobert <joey@joeyrobert.org>
2015-12-06 21:59:59 -0500

    Update mocha to generate tests, disable timeouts

--- test/board.test.js
+++ test/board.test.js
@@ -5,21 +5,16 @@ const fs = require('fs');
 const Board = require('../src/board');
 
 describe('board', () => {
-    it('passes the example test', () => {
-        expect(3).to.eql(3);
+    let board;
+    let epd = fs.readFileSync('suites/epd/perftsuite.epd', 'utf8');
+    let fens = epd.split('\n').map(line => line.split(';')[0].trim());
+
+    beforeEach(() => {
+        board = new Board();
     });
 
-    it('can create a new board', () => {
-        let board = new Board();
-    });
-
-    it('can set fen and get back the same fen', () => {
-        let board = new Board();
-        let epd = fs.readFileSync('suites/epd/perftsuite.epd', 'utf8');
-        let fens = epd.split('\n')
-            .map(line => line.split(';')[0].trim());
-
-        fens.forEach(fen => {
+    fens.forEach(fen => {
+        it(`can set and get fen ('${fen}')`, () => {
             board.fen = fen;
             expect(board.fen).to.equal(fen);
         });
--- test/perft.test.js
+++ test/perft.test.js
@@ -6,22 +6,25 @@ const Board = require('../src/board');
 const perft = require('../src/perft');
 
 describe('perft', () => {
-    it('can set compute perfts', () => {
-        let board = new Board();
-        let epd = fs.readFileSync('suites/epd/perftsuite.epd', 'utf8');
-        let perftTests = epd.split('\n').map(line => line.split(';'));
+    let board;
+    let epd = fs.readFileSync('suites/epd/perftsuite.epd', 'utf8');
+    let perftTests = epd.split('\n').map(line => line.split(';'));
 
-        perftTests.forEach(perftTest => {
-            let fen = perftTest[0].trim();
-            console.log('fen', fen);
-            board.fen = fen;
+    beforeEach(() => {
+        board = new Board();
+    });
 
-            for (let i = 1; i < perftTest.length; i++) {
-                let parts = perftTest[i].split(' ');
-                let depth = parseInt(parts[0].slice(1), 10);
-                let count = parseInt(parts[1], 10);
+    perftTests.forEach(perftTest => {
+        let fen = perftTest[0].trim();
+
+        for (let i = 1; i < perftTest.length; i++) {
+            let parts = perftTest[i].split(' ');
+            let depth = parseInt(parts[0].slice(1), 10);
+            let count = parseInt(parts[1], 10);
+            it(`verify perft('${fen}', ${depth}) equals ${count}`, () => {
+                board.fen = fen;
                 expect(perft(board, depth)).to.equal(count);
-            }
-        });
+            });
+        }
     });
 });
