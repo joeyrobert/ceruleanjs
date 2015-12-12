724746ea5ed6e430c7a1ebc431a6d188a26ffddc
joeyrobert <joey@joeyrobert.org>
2015-12-12 11:15:56 -0500

    Perft table backed by hash

--- src/hash_table.js
+++ src/hash_table.js
@@ -1,17 +1,24 @@
 'use strict';
 
 module.exports = class HashTable {
-    constructor(exponent = 16) {
-        this.exponent = exponent;
-        this.size = Math.pow(2, 16);
+    constructor(exponent) {
+        this.exponent = exponent || 16;
+        this.size = Math.pow(2, this.exponent);
         this.table = new Array(this.size);
     }
 
+    add(key, depth, value) {
+        let oldValue = this.get(key) || {};
+        oldValue[depth] = value;
+        this.set(key, oldValue);
+    }
+
     set(key, value) {
-        this.table[key % this.size] = value;
+        this.table[key % this.size] = [key, value];
     }
 
     get(key) {
-        return this.table[key % this.size];
+        let value = this.table[key % this.size];
+        return value && value[0] === key ? value[1] : undefined;
     }
 };
