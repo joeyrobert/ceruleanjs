396956648cda45896375f95e8a355b017373c1ae
joeyrobert <joey@joeyrobert.org>
2015-12-26 18:12:43 -0500

    Use utils rankFileToIndex in zobrist generation

--- src/zobrist.js
+++ src/zobrist.js
@@ -2,8 +2,7 @@
 
 const MersenneTwister = require('mersennetwister');
 const constants = require('./constants');
-
-var rankFileToIndex = (rankIndex, fileIndex) => rankIndex * 15 + fileIndex + 17;
+const utils = require('./utils');
 
 function generateZobristKeys() {
     var mt = new MersenneTwister(3141592654);
@@ -17,7 +16,7 @@ function generateZobristKeys() {
 
     for (rankIndex = 1; rankIndex <= 8; rankIndex++) {
         for (fileIndex = 1; fileIndex <= 8; fileIndex++) {
-            index = rankFileToIndex(fileIndex, rankIndex);
+            index = utils.rankFileToIndex(fileIndex, rankIndex);
 
             for (turn = 0; turn < 2; turn++) {
                 for (pieceIndex = 0; pieceIndex < pieceKeys.length; pieceIndex++) {
